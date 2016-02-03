9f281675be7d1f99ce0c2f32a3dc2fdbd6d4e874
Emma Sax <emma.sax4@gmail.com>
2016-02-02 20:57:47 -0600

    Adding first part of Bonfires

---
+++ Bonfires/src/LightGenerator.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+
+public class LightGenerator {
+	private static ArrayList<Integer> queue = new ArrayList<Integer>();
+	
+	//Effects the global grid by looking for torches and "spreading" the light
+	public static void generateLight(int[][] grid){
+		
+	}
+	
+	public static void findTorches() {
+		
+	}
+	
+	public static void spreadLight() {
+		
+	}
+}
