77df7accdb1dc2e895551fe16265c997a2598eee
Emma Sax <emma.sax4@gmail.com>
2016-02-04 08:58:18 -0600

    Fixing more spacing; don't know why GitHub hates tabs but loves spaces...

--- Bonfires/src/BonfireTest.java
+++ Bonfires/src/BonfireTest.java
@@ -3,24 +3,24 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class BonfireTest {
-	
-	int[][] grid;
-	
-	@Before
-	public void before() {
-		grid = new int[3][4];
-		grid[1][0] = 15;
-		grid[1][1] = -1;
-	}
+    
+    int[][] grid;
+    
+    @Before
+    public void before() {
+        grid = new int[3][4];
+        grid[1][0] = 15;
+        grid[1][1] = -1;
+    }
 
-	@Test
-	public void test() {
-		LightGenerator.generateLight(grid);
-		assertEquals(15, grid[1][0]);
-		assertEquals(-1, grid[1][1]);
-		assertEquals(11, grid[1][2]);
-		assertEquals(10, grid[1][3]);
-		assertEquals(11, grid[2][3]);
-	}
+    @Test
+    public void test() {
+        LightGenerator.generateLight(grid);
+        assertEquals(15, grid[1][0]);
+        assertEquals(-1, grid[1][1]);
+        assertEquals(11, grid[1][2]);
+        assertEquals(10, grid[1][3]);
+        assertEquals(11, grid[2][3]);
+    }
 
 }
--- Bonfires/src/LightGenerator.java
+++ Bonfires/src/LightGenerator.java
@@ -2,18 +2,18 @@ import java.util.ArrayList;
 
 
 public class LightGenerator {
-	private static ArrayList<Integer> queue = new ArrayList<Integer>();
-	
-	//Effects the global grid by looking for torches and "spreading" the light
-	public static void generateLight(int[][] grid){
-		
-	}
-	
-	public static void findTorches() {
-		
-	}
-	
-	public static void spreadLight() {
-		
-	}
+    private static ArrayList<Integer> queue = new ArrayList<Integer>();
+    
+    //Effects the global grid by looking for torches and "spreading" the light
+    public static void generateLight(int[][] grid){
+        
+    }
+    
+    public static void findTorches() {
+        
+    }
+    
+    public static void spreadLight() {
+        
+    }
 }
